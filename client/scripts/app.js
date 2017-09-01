000c0bdae72a700ab7083473ae77c8d160e2933e
Hack Reactor Student <student@hackreactor.com>
2017-09-01 15:30:09 -0700

    chatroom behavior tests passing

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,6 +8,27 @@ class App {
   send(message) {
     $.ajax({type: 'POST', data: message});
   }
+  fetch() {
+    $.ajax({type: 'GET'});
+  }
+  clearMessages() {
+    $('#chats').html('');
+  }
+  renderMessage(message) {
+    // console.log("Chats children was: ", $('#chats').children());
+    $('#chats').append('<div>' + message.text +'</div>');
+
+    // console.log("Chats children is now: ", $('#chats').children());
+  }
+  renderRoom(room) {
+    //console.log("RoomSelect children was: ", $('#roomSelect').children());
+    $('#roomSelect').append('<div>' + room + '<div>');
+    //console.log("RoomSelect children now is: ", $('#roomSelect').children());
+  }
+
+  handleUsernameClick() { 
+
+  }
 }
 
 var app = new App();
